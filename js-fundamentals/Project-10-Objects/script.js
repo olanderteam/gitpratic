5076d6019e6f79881d129fc96f71a0fba25cf6c4
Gabe <gaolander@gmail.com>
2026-02-03 21:13:54 -0300

    project 10 update

--- js-fundamentals/Project-10-Objects/script.js
+++ js-fundamentals/Project-10-Objects/script.js
@@ -1,7 +1,44 @@
 
 const person = {
-    
+ name: "Luis",
+ age: 24,
+ isStudent: true,
+};
 
+console.log(person.name);
+console.log(person.age);
+
+console.log(person["isStudent"]);
+
+person.city = "Miami";
+console.log(person);
+
+person.age = 25;
+console.log(person);
+
+delete person.isStudent;
+console.log(person);
+
+for (let key in person)
+console.log("key", key);
+
+
+for (let key in person) {
+    console.log(key, person[key]);
 }
 
+console.log(Object.keys(person)); 
+console.log(Object.values(person));
 
+console.log(Object.entries(person));
+
+const food = {
+    fruit: 'banana',
+    snack: 'chocolate',
+    main: {
+        vegan: 'salad',
+        popular: 'steak'
+    }
+}
+
+console.log(food.main.vegan);
