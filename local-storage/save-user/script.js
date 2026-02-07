0ee54a18fc237e1f55dd105a03513ca027f41b5c
Gabriel Olander <thesvik@gmail.com>
2026-02-07 19:38:56 -0300

    update saver-user

--- local-storage/save-user/script.js
+++ local-storage/save-user/script.js
@@ -1,15 +1,30 @@
 const nameInput = document.getElementById("nameInput");
-const saveBtn = document.getElementById("saveBtn")
+const saveBtn = document.getElementById("saveBtn");
 const userDisplay = document.getElementById("userDisplay");
 
-const savedName = localStorage.getItem("username")
-if (savedName){
-    userDisplay.textContent = savedName;
+const savedName = localStorage.getItem("username");
+if (savedName) {
+    userDisplay.textContent = `Hi again, ${savedName}`;
 }
 
-saveBtn.addEventListener("click", () => {
-    const value = nameInput.value;
-    localStorage.setItem("username", value);
-    userDisplay.textContent = value;
+if (saveBtn) {
+    saveBtn.addEventListener("click", () => {
+        const value = nameInput.value;
+        localStorage.setItem("username", value);
+        userDisplay.textContent = value;
+    });
+}
 
+const toggleThemeBtn = document.getElementById("toggleThemeBtn");
+const body = document.body;
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+    body.classList.add("dark");
+}
+
+toggleThemeBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  let currentTheme = body.classList.contains("dark") ? "dark" : "light";
+  localStorage.setItem("theme", currentTheme);
 });
